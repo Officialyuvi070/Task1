da23ecc6282353649a9ecb7900ccd7a1a5ffeefd
YUVRAJ KUMAR ARORA <110113664+Officialyuvi070@users.noreply.github.com>
2023-03-28 19:40:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,39 @@
+#Examples of List.
+#1. Declare the List.
+yuvi=[256,"ECE",56.45,"y",593]
+print(yuvi)
+#2. How to find lenth of list by using len() function.
+print(len(yuvi))
+#3. how to indexing work in a list.
+print(yuvi[1])# +ve indexing left to right.
+print(yuvi[-1])# -ve indexing right to left.
+print(yuvi[3])
+
+##Examples of Tuple.
+#1. Declare the Tuple.
+tup1=(786,"rahul",13.43,"a",32)
+print(tup1)
+#2. How to find lenth of tuple by using len() function.
+print(len(tup1))
+#3. how to indexing work in a tuple.
+print(tup1[1])# +ve indexing left to right.
+print(tup1[-1])# -ve indexing right to left.
+print(tup1[3])
+
+#Examples of Dictionary.
+#1. Declare the Dictionary.
+dict={"Employe Name":"YUVRAJ KUMAR ARORA","Employe Id": 593,"Pincode": 144002,}
+print(dict)
+#2. How to find lenth of dictionary by using len() function.
+print(len(dict))
+#3. how to indexing work in a dictionary with key.
+print(dict["Employe Id"])
+print(dict["Pincode"])
+print(dict["Employe Name"])
+
+#Examples of Set.
+#1. Declare the set.
+rishi={999,"rishu",12.78,"u",234}
+print(rishi)
+#2. How to find lenth of set by using len() function.
+print(len(rishi))
